b203eeb192faa188c7d58af6c940f8d0efb74461
Asadbek <asadbek010azamjonov@gmail.com>
2025-03-22 15:51:47 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import CardComp from "./components/cardComp"
+import axios from "axios";
+
+
+function App() {
+  const [data, setData] = useState([]);
+  async function getData() {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    
+      setData(response.data);
+      console.log(response);
+    } catch (error) {
+      
+    }
+  }
+
+
+useEffect(() => {
+  getData()
+}, [])
+
+  return (<>
+    <CardComp data={data} />
+    </>
+  )
+}
+
+export default App
