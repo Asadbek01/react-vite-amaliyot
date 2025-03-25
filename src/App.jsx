7a70f683431da8bfd997f507d60155ec6a34ab95
Asadbek <asadbek010azamjonov@gmail.com>
2025-03-25 14:59:16 +0500

    task finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import { useEffect, useState } from "react";
-import CardComp from "./components/cardComp"
-import axios from "axios";
-
+import { useEffect, useState } from 'react';
+import CardComp from './components/cardComp';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState([]);
-  async function getData() {
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-    
-      setData(response.data);
-      console.log(response);
-    } catch (error) {
-      
-    }
-  }
+	const [data, setData] = useState([]);
+	async function getData() {
+		try {
+			const response = await axios.get(
+				'https://jsonplaceholder.typicode.com/todos'
+			);
 
+			setData(response.data);
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-useEffect(() => {
-  getData()
-}, [])
+	useEffect(() => {
+		getData();
+	}, []);
 
-  return (<>
-    <CardComp data={data} />
-    </>
-  )
+	return (
+		<>
+			<CardComp data={data} />
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/cardComp.jsx
+++ src/components/cardComp.jsx
@@ -1,35 +1,31 @@
-
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+function CardComp({ data }) {
+	return (
+		<div className='container d-flex flex-wrap gap-3'>
+			{data.map(val => {
+				console.log(val);
 
-function CardComp({data}) {
-
-  
-
-
-
-  return (
-      <>
-      {
-        data.map(val => {
-          return (
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://mdbootstrap.com/img/Photos/Others/water-lily.jpg" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          )
-        })
-      }
-    </>
-  )
+				return (
+					<Card style={{ width: '18rem' }}>
+						<Card.Img
+							variant='top'
+							src='https://mdbootstrap.com/img/Photos/Others/water-lily.jpg'
+						/>
+						<Card.Body>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+							<Button variant='primary'>Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				);
+			})}
+		</div>
+	);
 }
 
-export default CardComp
+export default CardComp;
